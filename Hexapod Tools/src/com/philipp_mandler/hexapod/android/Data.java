ae64cf3e3eb278e502c32d8573616c762d54d5e3
Philipp <info@philipp-mandler.com>
2013-01-17 22:26:56 +0100

    Hexapod Tools App deletet (outdated und unused)

--- Hexapod Tools/src/com/philipp_mandler/hexapod/android/Data.java
+++
@@ -1,6 +0,0 @@
-package com.philipp_mandler.hexapod.android;
-
-public class Data {
-	static final double upperLeg = 175;
-	static final double lowerLeg = 240;
-}
--- Hexapod Tools/src/com/philipp_mandler/hexapod/android/MainActivityListener.java
+++
@@ -1,7 +0,0 @@
-package com.philipp_mandler.hexapod.android;
-
-import android.view.View;
-
-public interface MainActivityListener {
-	public void onRadioButtonClicked(View view);
-}
